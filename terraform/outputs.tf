67772a6878cdc0f40fdc4e5f47ef0928e67fc290
Veerababu Paidikondala <pveerababudevops@gmail.com>
2026-08-06 11:08:49 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "instance_id" {
+  value = aws_instance.web.id
+}
+
+output "public_ip" {
+  value = aws_eip.web.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.web.private_ip
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
